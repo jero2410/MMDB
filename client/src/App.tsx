469fd61965df851c2fab10537db2e26d56b146b2
Jerome Nabil <jeromenabil9@gmail.com>
2026-08-24 14:40:13 +0300

    Implementing footer and routes for is buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { Footer } from "./components/footer";
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router";
+import { AboutScreen } from "./screens/AboutScreen";
+import { TermsScreen } from "./screens/TermsScreen";
+import { PrivacyPolicyScreen } from "./screens/PrivacyPolicyScreen";
+import { HelpScreen } from "./screens/HelpScreen";
 
 function App() {
-
   return (
-    <>
-    
-            
-    </>
-  )
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* header */}
+      <Routes> 
+        <Route path="/About" element={<AboutScreen />} />
+        <Route path="/Terms" element={<TermsScreen />} />
+        <Route path="/Privacy-Policy" element={<PrivacyPolicyScreen />} />
+        <Route path="/Help" element={<HelpScreen />} />
+      </Routes>
+
+      <Box component="main" sx={{ flex: 1 }}>
+        {/* Your page content */}
+      </Box>
+
+      <Footer />
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/footer.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Container } from "@mui/material";
+import MMDBLogo from "../assets/MMDB.svg";
+import { Link } from "react-router";
+
+export function Footer() {
+  return (
+    <>
+      <Box
+        component="footer"
+        sx={{
+          height: "214px",
+          backgroundColor: "#003055",
+          display: "flex",
+          alignItems: "center", // Vertically centers the content
+          flexDirection: "column",
+          justifyContent: "center",
+          gap: 3,
+          color: "white",
+        }}
+      >
+        <Button component={Link} to="/">
+          <Box component="img" src={MMDBLogo} alt="MMDB" />
+        </Button>
+        <Container
+          maxWidth="md"
+          sx={{
+            display: "flex",
+            justifyContent: "center", // Horizontally centers the buttons
+            gap: 2, // Spaces between buttons
+            flexWrap: "wrap", // Wraps nicely on mobile screens
+          }}
+        >
+          <Button component={Link} to="/About" variant="text" color="inherit">
+            {" "}
+            About{" "}
+          </Button>
+          <Button component={Link} to="/Terms" variant="text" color="inherit">
+            Terms of use
+          </Button>
+          <Button
+            component={Link}
+            to="/Privacy-Policy"
+            variant="text"
+            color="inherit"
+          >
+            Privacy Policy
+          </Button>
+          <Button component={Link} to="/Help" variant="text" color="inherit">
+            Help
+          </Button>
+        </Container>
+        <Box>© 2026 MMDB. All rights reserved.</Box>
+      </Box>
+    </>
+  );
+}
