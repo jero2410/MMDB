43ea007218a5c3f0ace7aaaeb9f05a6c00f5eb7b
Jerome Nabil <jeromenabil9@gmail.com>
2026-08-24 16:50:43 +0300

    using mui TextFiled component instead of input

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,14 +1,9 @@
-import Button from "@mui/material/Button";
-import { AppBar, Toolbar, Stack, FormControl } from "@mui/material";
-import { Link } from "react-router"
+import { AppBar, Toolbar, Stack, FormControl, TextField, Button } from "@mui/material";
+import { Link } from "react-router";
 
 export function Header() {
   return (
-    <AppBar
-      position="static"
-      color="inherit"
-
-    >
+    <AppBar position="static" color="inherit">
       <Toolbar
         sx={{
           display: "flex",
@@ -23,13 +18,17 @@ export function Header() {
           spacing={7}
           sx={{ alignItems: "center", flexWrap: "nowrap" }}
         >
-          <Link to={"/"} ><img src="/src/assets/MMDB.svg" alt="Logo" style={{ height: "20" }} /></Link>
+          <Link to={"/"}>
+            <img
+              src="/src/assets/MMDB.svg"
+              alt="Logo"
+              style={{ height: "20" }}
+            />
+          </Link>
           <Stack direction="row" spacing={2}>
             <Button color="inherit">Home</Button>
             <Button color="inherit">Genre</Button>
-            <FormControl size="small">
-              
-            </FormControl>
+            <FormControl size="small"></FormControl>
           </Stack>
         </Stack>
         {/* Center/Right Side: Search Input and Auth Buttons */}
@@ -38,17 +37,35 @@ export function Header() {
           spacing={2}
           sx={{ flexWrap: "nowrap", alignItems: "center" }}
         >
-          <input
-            type="text"
+          <TextField
             placeholder="Search"
-            style={{
-              padding: "6px 12px",
-              borderRadius: "20px",
-              border: "1px solid #ccc",
+            variant="outlined"
+            size="small"
+
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "20px",
+                "& fieldset": {
+                  borderColor: "#ccc",
+                },
+                "&:hover fieldset": {
+                  borderColor: "#ccc",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#1976d2", // change to your focus color
+                },
+              },
+              "& .MuiOutlinedInput-input": {
+                padding: "6px 12px",
+              },
             }}
           />
-          <Button component={Link} to="/Signup" variant="text">Sign up</Button>
-          <Button component={Link} to="/Login" variant="contained">Login</Button>
+          <Button component={Link} to="/Signup" variant="text">
+            Sign up
+          </Button>
+          <Button component={Link} to="/Login" variant="contained">
+            Login
+          </Button>
         </Stack>
       </Toolbar>
     </AppBar>
