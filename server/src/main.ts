7a8240160e021a8d12a8e17da6c0a9dcd1ea5853
Jerome Nabil <jeromenabil9@gmail.com>
2026-08-31 13:52:15 +0300

    Update main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: process.env.CLIENT_URL });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Auto-transform payloads to DTO instances
