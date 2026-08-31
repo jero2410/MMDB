6f5fcbcc8ab3c0a69badca66e590d67750177989
Jerome Nabil <jeromenabil9@gmail.com>
2026-08-31 13:41:00 +0300

    Update states.type.ts

--- client/src/types/states.type.ts
+++ client/src/types/states.type.ts
@@ -5,4 +5,27 @@ export const Status = {
   Ready: "ready",
 } as const;
 
-export type Status = (typeof Status)[keyof typeof Status];
+export type Status =
+  (typeof Status)[keyof typeof Status];
+
+export type State<T> =
+  | {
+      status: typeof Status.Loading;
+      data?: undefined;
+      error?: undefined;
+    }
+  | {
+      status: typeof Status.Error;
+      error: Error;
+      data?: undefined;
+    }
+  | {
+      status: typeof Status.Empty;
+      data?: undefined;
+      error?: undefined;
+    }
+  | {
+      status: typeof Status.Ready;
+      data: T;
+      error?: undefined;
+    };
