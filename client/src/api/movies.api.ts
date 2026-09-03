bc576c83e9b355c04975205d8fe1870a9366a412
Jerome Nabil <jeromenabil9@gmail.com>
2026-09-03 10:52:08 +0300

    Fix base url

--- client/src/api/movies.api.ts
+++ client/src/api/movies.api.ts
@@ -1,6 +1,6 @@
 import type { MoviesResponse } from "../types/moviesResponse.type";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "";
 
 export async function fetchMovies(page: number, limit: number = 8) {
   const response = await fetch(
