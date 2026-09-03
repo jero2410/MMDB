767766b8e68bdb0cde4d590e80dce6356b947a45
Jerome Nabil <160172199+jero2410@users.noreply.github.com>
2026-09-03 12:10:30 +0300

    Revert "Update header and footer styling to be identical to user story"

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,98 +1,72 @@
-import {
-  AppBar,
-  Toolbar,
-  Stack,
-  TextField,
-  Button,
-  Container,
-  InputAdornment,
-} from "@mui/material";
+import { AppBar, Toolbar, Stack, TextField, Button } from "@mui/material";
 import { Link } from "react-router";
-import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
 export function Header() {
   return (
-    <AppBar
-      position="static"
-      color="inherit"
-      elevation={0}
-      sx={{ borderBottom: "1px solid", borderColor: "#E6E6E6" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-            flexWrap: "nowrap",
-          }}
+    <AppBar position="static" color="inherit">
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between", // Pushes content to far left and far right
+          alignItems: "center",
+          flexWrap: "nowrap", // Forces everything to be on one line
+        }}
+      >
+        {/* Left Side: Logo and Navigation links */}
+        <Stack
+          direction="row"
+          spacing={7}
+          sx={{ alignItems: "center", flexWrap: "nowrap" }}
         >
-          {/* Left Side: Logo and Navigation links */}
-          <Stack
-            direction="row"
-            spacing={7}
-            sx={{ alignItems: "center", flexWrap: "nowrap" }}
-          >
-            <Link to={"/"}>
-              <img
-                src="/src/assets/MMDB.svg"
-                alt="Logo"
-                style={{ height: "20" }}
-              />
-            </Link>
-            <Stack direction="row" spacing={2}>
-              <Button component={Link} to="/" color="inherit">
-                Home
-              </Button>
-              <Button color="inherit">Genre</Button>
-            </Stack>
-          </Stack>
-          {/* Center/Right Side: Search Input and Auth Buttons */}
-          <Stack
-            direction="row"
-            spacing={2}
-            sx={{ flexWrap: "nowrap", alignItems: "center" }}
-          >
-            <TextField
-              placeholder="Search"
-              variant="outlined"
-              size="small"
-              slotProps={{
-                input: {
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SearchRoundedIcon sx={{ color: "#697586" }} />
-                    </InputAdornment>
-                  ),
-                },
-              }}
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  borderRadius: "20px",
-                  "& fieldset": {
-                    borderColor: "#E5E5E5",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#ccc",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#1976d2", // change to your focus color
-                  },
-                },
-                "& .MuiOutlinedInput-input::placeholder": {
-                  color: "#697586",
-                },
-              }}
+          <Link to={"/"}>
+            <img
+              src="/src/assets/MMDB.svg"
+              alt="Logo"
+              style={{ height: "20" }}
             />
-            <Button component={Link} to="/signup" variant="text">
-              Sign up
-            </Button>
-            <Button component={Link} to="/login" variant="contained">
-              Login
-            </Button>
+          </Link>
+          <Stack direction="row" spacing={2}>
+            <Button component={Link} to="/" color="inherit">Home</Button>
+            <Button color="inherit">Genre</Button>
           </Stack>
-        </Toolbar>
-      </Container>
+        </Stack>
+        {/* Center/Right Side: Search Input and Auth Buttons */}
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ flexWrap: "nowrap", alignItems: "center" }}
+        >
+          <TextField
+            placeholder="Search"
+            variant="outlined"
+            size="small"
+
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                borderRadius: "20px",
+                "& fieldset": {
+                  borderColor: "#ccc",
+                },
+                "&:hover fieldset": {
+                  borderColor: "#ccc",
+                },
+                "&.Mui-focused fieldset": {
+                  borderColor: "#1976d2", // change to your focus color
+                },
+              },
+              "& .MuiOutlinedInput-input": {
+                padding: "6px 12px",
+              },
+            }}
+          />
+          <Button component={Link} to="/signup" variant="text">
+            Sign up
+          </Button>
+          <Button component={Link} to="/login" variant="contained">
+            Login
+          </Button>
+        </Stack>
+      </Toolbar>
     </AppBar>
   );
 }
